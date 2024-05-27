ad001e49d36b030afacb8058aefea87668bf085d
Rui Dias <rui.dias10@gmail.com>
2024-05-27 15:58:05 +0100

    Update 2024_05_24_142001_create_notes_table.php

--- Laravel/app-litenotes-essential-linkedin/database/migrations/2024_05_24_142001_create_notes_table.php
+++ Laravel/app-litenotes-essential-linkedin/database/migrations/2024_05_24_142001_create_notes_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            // Cria um slug
+            $table->uuid('uuid');
             // Cria chave estrangeira, tabela user, campo id
             $table->foreignId('user_id')->constrained();
             $table->string('title');
